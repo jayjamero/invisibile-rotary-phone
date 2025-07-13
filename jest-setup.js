d77c485f7dacb7ec9e1bc574257555ef11c08ab0
jayjamero <jayjamero@gmail.com>
2025-07-14 02:50:20 +1000

    feat: Footer Component (passing)
    Test failing due to broken config
    - add polyfill to jsdom
    - config for rending tsx components
    - mock matchMedifa for jest

--- jest-setup.js
+++ jest-setup.js
@@ -2,3 +2,13 @@ require("@testing-library/jest-dom");
 const React = require("react");
 
 global.React = React;
+
+global.matchMedia =
+    global.matchMedia ||
+    function () {
+        return {
+            matches: false,
+            addListener: function () {},
+            removeListener: function () {},
+        };
+    };
