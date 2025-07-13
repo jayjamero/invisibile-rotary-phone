17d264ad877fe9918bc1c315980e93ecb7747039
jayjamero <jayjamero@gmail.com>
2025-07-14 01:53:21 +1000

    feat: Add RTL + Jest with basic config

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    preset: "ts-jest",
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": [
+            "babel-jest",
+            {
+                presets: [
+                    [
+                        "next/babel",
+                        {
+                            /*
+                             ** Runtime is being set to "classic", overriding the default
+                             ** https://github.com/vercel/next.js/blob/canary/packages/next/src/build/babel/preset.ts#L86
+                             */
+                            "preset-react": { runtime: "automatic" },
+                        },
+                    ],
+                ],
+            },
+        ],
+    },
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+    moduleFileExtensions: ["js", "ts", "tsx"],
+};
