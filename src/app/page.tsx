8141c69c2185ea78c21ec33f9a3f8971dcb3d068
jayjamero <jayjamero@gmail.com>
2025-07-14 20:00:28 +1000

    feat: Setup homepage to trigger modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Button, HStack } from '@chakra-ui/react';
+import { Button, HStack, VStack, Text, Box, Container, Flex } from '@chakra-ui/react';
 
-import { ColorModeButton } from '@/components/ui/ColorMode';
 import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
+import { UserFormTrigger } from '@/components/ui/FormModal';
+
 import styles from './page.module.css';
 
 export default function Home() {
@@ -12,23 +13,59 @@ export default function Home() {
         <div className={styles.page}>
             <Header />
             <main className={styles.main}>
-                <HStack>
-                    <Button variant="outline" size="xl" onClick={() => window.alert('no!')}>
-                        No
-                    </Button>
-                    <Button size="xl" onClick={() => window.alert('yes!')}>
-                        Yes
-                    </Button>
-                    <ColorModeButton />
-                </HStack>
-                <br />
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                <Box
+                    bg={{ base: 'rgba(255, 255, 255, 0.8)', _dark: 'rgba(0, 0, 0, 0.8)' }}
+                    display="flex"
+                    justifyContent="center"
+                    padding={{ base: '6', md: '10' }}
+                    margin={{ base: '0', md: '0' }}
                 >
-                    Deploy now
-                </a>
+                    <Container maxW="8xl">
+                        <Flex justify="space-between" align="stretch" textAlign="justify">
+                            <VStack gap={6}>
+                                <HStack>
+                                    <Text fontSize="2xl" fontWeight="bold">
+                                        Rick and Morty Challenge
+                                    </Text>
+                                </HStack>
+
+                                <VStack gap={3} textAlign="center">
+                                    <Text>
+                                        Wow, I really Cronenberged up the whole place, huh Morty? Just a bunch a
+                                        Cronenbergs walkin' around.
+                                    </Text>
+
+                                    <Text>
+                                        Listen to your sister, Morty. To live is to risk it all, otherwise you're just
+                                        an inert chunk of randomly assembled molecules drifting wherever the universe
+                                        blows you.
+                                    </Text>
+
+                                    <Text>
+                                        He's not a hot girl. He can't just bail on his life and set up shop in someone
+                                        else's.
+                                    </Text>
+
+                                    <Text>Weddings are basically funerals with cake.</Text>
+
+                                    <Text>
+                                        You're missing the point Morty. Why would he drive a smaller toaster with
+                                        wheels? I mean, does your car look like a smaller version of your house? No.
+                                    </Text>
+                                </VStack>
+                            </VStack>
+                        </Flex>
+                        <VStack mt={10} align="center">
+                            <UserFormTrigger>
+                                <Button size="md" variant="surface" px={2} py={6} colorPalette="teal">
+                                    <Text fontSize={{ base: 'sm', md: 'md' }} fontWeight="semibold">
+                                        Find out more
+                                    </Text>
+                                </Button>
+                            </UserFormTrigger>
+                        </VStack>
+                    </Container>
+                </Box>
             </main>
             <Footer />
         </div>
