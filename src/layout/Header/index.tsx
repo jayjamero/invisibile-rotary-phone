c2a3f7b452e403dc6c826c0e0ede89714440c333
jayjamero <jayjamero@gmail.com>
2025-07-14 14:17:40 +1000

    feat: Implement HeaderUI (passing)

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Box, Container, Flex, Text, Link, Image, Button, Stack } from '@chakra-ui/react';
+import { ColorModeButton } from '../../components/ui/color-mode';
+
+/* To aid in implementing the menu items in mobile drawer */
+const menuItems = (
+    <>
+        <Button asChild variant="ghost" px={2} py={6}>
+            <Link href="/information">
+                <Text fontSize={{ base: 'sm', md: 'md' }} fontWeight="semibold">
+                    Information
+                </Text>
+            </Link>
+        </Button>
+        <Button asChild size="md" variant="surface" px={2} py={6} colorPalette="teal">
+            <Link href="/information">
+                <Text fontSize={{ base: 'sm', md: 'md' }} fontWeight="semibold">
+                    Get Started
+                </Text>
+            </Link>
+        </Button>
+        <ColorModeButton />
+    </>
+);
+
+const Header = () => {
+    return (
+        <Box w="full" boxShadow="md" display="flex" justifyContent="center">
+            <Container maxW="8xl" padding={{ base: '2', md: '4' }}>
+                <Flex justify="space-between" align="stretch">
+                    {/* Logo */}
+                    <Link href="/">
+                        <Flex gap={2} align="center" justify="flex-start">
+                            <Box position="relative">
+                                <Image
+                                    src="/leonardo-logo.svg"
+                                    alt="Leonardo AI logo"
+                                    width={{ base: 20 }}
+                                    aspectRatio={1 / 1}
+                                />
+                            </Box>
+                            <Text display={{ smDown: 'none' }} fontSize={{ base: 'sm', md: 'md' }} fontWeight="bold">
+                                Rick and Morty
+                            </Text>
+                        </Flex>
+                    </Link>
+                    {/* Menu items */}
+                    <Flex gap={3} align="center" justify="flex-end">
+                        {menuItems}
+                    </Flex>
+                </Flex>
+            </Container>
+        </Box>
+    );
+};
+
+export default Header;
